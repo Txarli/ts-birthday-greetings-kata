c434e2d4ae72e6d4b29fec6c5761e403623b9317
Aitor Urrutia <aitor.urrutia@biko2.com>
2020-02-19 13:00:11 +0100

    Mueve fichero

--- test/Acceptance.test.ts
+++ test/Acceptance.test.ts
@@ -1,10 +1,10 @@
-import { BirthdayService } from '../src/BirthdayService';
 import { OurDate } from '../src/domain/model/OurDate';
 import { FileEmployeesRepository } from '../src/infrastructure/FileEmployeesRepository';
 import {
   Message,
   SmtpGreetingsRepository,
 } from '../src/infrastructure/SmtpGreetingsRepository';
+import { BirthdayService } from '../src/services/BirthdayService';
 
 describe('Acceptance', () => {
   const SMTP_PORT = 25;
